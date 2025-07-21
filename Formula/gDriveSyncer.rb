738c292a9a8fa13f96acc461e869b1af9fba994b
azinc28 <ajinkya.singh@alaanpay.com>
2025-07-21 20:13:39 +0530

    first release

---
+++ Formula/gDriveSyncer.rb
@@ -0,0 +1,17 @@
+class Gdrivesync < Formula
+  desc "Command-line tool for synchronizing files with Google Drive"
+  homepage "https://github.com/a-ZINC/gDriveSyncer"
+  url "https://github.com/a-ZINC/gDriveSyncer/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "bd203cc2729c8801670eeae45033738beacbf67ece639597c6b16356b06b3cec"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-o", bin/"gdrivesync", "."
+  end
+
+  test do
+    system "#{bin}/gdrivesync", "--help"
+  end
+end
